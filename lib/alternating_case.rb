ab136c36ff481a7f92692c8a25456a0a766827b4
Brad Phillips <bjrp@protonmail.com>
2020-04-27 23:42:16 +0100

    Passes second unit test

--- lib/alternating_case.rb
+++ lib/alternating_case.rb
@@ -2,9 +2,9 @@ def alternating_case(input)
   alternated_input = []
   input.split('').each_with_index do |letter, index|
     if index.even? 
-      x << letter
+      alternated_input << letter
     else
-      x << letter.downcase
+      alternated_input << letter.downcase
     end
   end
   alternated_input.join
--- spec/alternating_case_spec.rb
+++ spec/alternating_case_spec.rb
@@ -4,4 +4,7 @@ describe 'alternating_case' do
   it 'returns AsCiI when passed ASCII' do
     expect(alternating_case('ASCII')).to eq('AsCiI')
   end
+  it 'returns 42 when passed 42' do
+    expect(alternating_case('42')).to eq('42')
+  end
 end
