b1690186a1bce80e1f2762e6e2ede20d85556384
Brad Phillips <bjrp@protonmail.com>
2020-04-30 21:45:28 +0100

    Adds second step of possible solution - splits arrays into alphabetical and non-alphabetical characters

--- lib/alternating_case.rb
+++ lib/alternating_case.rb
@@ -2,11 +2,25 @@ def alternating_case(input)
   alternated_input = []
   input.split('').each_with_index do |letter, index|
     alternated_input << [letter, index]
-    # if index.even? 
+  end
+  sortables =[]
+  irrelevants =[]
+  alternated_input.each do |el|
+    if el[0].match(/[a-zA-Z]/)
+      sortables << el
+    else
+      irrelevants << el
+    end
+  end
+  print sortables
+  puts "\n"
+  puts '-----------'
+  print irrelevants
+end
+
+
+ # if index.even? 
     #   alternated_input << letter.upcase
     # else
     #   alternated_input << letter.downcase
     # end
-  end
-  alternated_input
-end
