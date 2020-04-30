0a0c81ccd87d5a7e07a1dd22551bd977c9e2219e
Brad Phillips <bjrp@protonmail.com>
2020-04-30 21:23:36 +0100

    First step of possible solution - creates array with indexes

--- lib/alternating_case.rb
+++ lib/alternating_case.rb
@@ -1,11 +1,12 @@
 def alternating_case(input)
   alternated_input = []
   input.split('').each_with_index do |letter, index|
-    if index.even? 
-      alternated_input << letter
-    else
-      alternated_input << letter.downcase
-    end
+    alternated_input << [letter, index]
+    # if index.even? 
+    #   alternated_input << letter.upcase
+    # else
+    #   alternated_input << letter.downcase
+    # end
   end
-  alternated_input.join
+  alternated_input
 end
--- spec/alternating_case_spec.rb
+++ spec/alternating_case_spec.rb
@@ -7,4 +7,7 @@ describe 'alternating_case' do
   it 'returns 42 when passed 42' do
     expect(alternating_case('42')).to eq('42')
   end
+  it 'returns #InClUdE <iOsTrEaM> when passed #include <iostream>' do
+    expect(alternating_case('#include <iostream>')).to eq('#InClUdE <iOsTrEaM>')
+  end
 end
