dc10bf32bf7e028a23af55cf2f38969892c2f7a8
Brad Phillips <bjrp@protonmail.com>
2020-05-02 20:52:22 +0100

    Adds tests

--- spec/alternating_case_spec.rb
+++ spec/alternating_case_spec.rb
@@ -10,4 +10,13 @@ describe 'alternating_case' do
   it 'returns #InClUdE <iOsTrEaM> when passed #include <iostream>' do
     expect(alternating_case('#include <iostream>')).to eq('#InClUdE <iOsTrEaM>')
   end
+  it 'returns A1b2 when passed A1B2' do
+    expect(alternating_case('A1B2')).to eq('A1b2')
+  end
+  it "returns 'ThIs ExAmPlE wIlL sTaRt WiTh UpPeRcAsE' when passed 'This example will start with uppercase'" do
+    expect(alternating_case('A1B2')).to eq('A1b2')
+  end
+  it 'returns lEaVe_My_SyMbOlS#!#&^%_aLoNe!!! when passed LEAVE_my_symbols#!#&^%_ALONE!!!' do
+    expect(alternating_case('LEAVE_my_symbols#!#&^%_ALONE!!!')).to eq('LeAvE_mY_sYmBoLs#!#&^%_AlOnE!!!')
+  end
 end
