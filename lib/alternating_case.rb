750b2aed7f4a61f942f4607b24086a1f5d601ab0
Brad Phillips <bjrp@protonmail.com>
2020-05-02 15:24:26 +0100

    Rejoins and sorts arrays

--- lib/alternating_case.rb
+++ lib/alternating_case.rb
@@ -1,24 +1,22 @@
 def alternating_case(input)
   alternated_input = []
   input.split('').each_with_index do |char, index|
-    alternated_input << [char.downcase, index]
+    alternated_input << [index, char.downcase]
   end
   sortables =[]
   irrelevants =[]
   alternated_input.each do |el|
-    if el[0].match(/[a-zA-Z]/)
+    if el[1].match(/[a-zA-Z]/)
       sortables << el
     else
       irrelevants << el
     end
   end
   sortables.each do |el|
-    el[0].upcase! if sortables.index(el).even?
+    el[1].upcase! if sortables.index(el).even?
   end
-  print sortables
-  puts "\n"
-  puts '------'
-  print irrelevants
+  result = sortables + irrelevants
+  result.sort
 end
 
 
